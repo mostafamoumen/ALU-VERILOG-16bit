7b596a48996e180c99cf786d16c8813a22f8cb49
AbdelrahmanEl-Koddosy <abdelrahman.el-koddosy05@eng-st.cu.edu.eg>
2025-11-23 13:36:36 +0200

    sprint1

--- ALU.v
+++ ALU.v
@@ -1,99 +1,107 @@
 module alu (
     input  [15:0] op1,op2,
-    input  [3:0] opcode,
+    input  [4:0] opcode,
+    input  cin ;
     output reg [15:0] out,
-    output reg logic_flag,cmp_flag,arith_flag,shift_flag
-);
+    output reg cf,nf,af,of,pf,zf
+ );
     always @(*) begin
-        out         = 16'b0;
-        logic_flag  = 1'b0;
-        cmp_flag    = 1'b0;
-        arith_flag  = 1'b0;
-        shift_flag  = 1'b0;
-
-        case(opcode)
-        4'b0000:begin
-            out=op1+op2;
-            arith_flag=1'b1;
-        end
-        4'b0001:begin
-            out=op1-op2;
-            arith_flag=1'b1;
-        end
-        4'b0010:begin
-            out=op1*op2;
-            arith_flag=1'b1;
-        end
-        4'b0011:begin
-            out=op1/op2;
-            arith_flag=1'b1;
-        end
-        4'b0100:begin
-            out=op1&op2;
-            logic_flag=1'b1;
-        end
-        4'b0101:begin
-            out=op1|op2;
-            logic_flag=1'b1;
-        end
-        4'b0110:begin
-            out=~(op1&op2);
-            logic_flag=1'b1;
-        end
-        4'b0111:begin
-            out=~(op1|op2);
-            logic_flag=1'b1;
-        end
-        4'b1000:begin
-            out=op1^op2;
-            logic_flag=1'b1;
-        end
-        4'b1001:begin
-            out=~(op1^op2);
-            logic_flag=1'b1;
-        end
-        4'b1010:begin//(comparison) will ask about complexity.better with cond. op.  ???
-            if (op1==op2) begin
-                out=16'b1;
-            end
-            else begin
-                out=16'b0;
-            end
-            cmp_flag=1'b1;
-
-        end
-        4'b1011:begin
-            if (op1>op2) begin
-                out=16'b10;
-            end
-            else begin
-                out=16'b0;
-            end
-            cmp_flag=1'b1;
+        out = 16'b0;
+        cf  = 1'b0;
+        zf  = 1'b0;
+        af  = 1'b0;
+        of  = 1'b0;
+        pf  = 1'b0;
+        nf  = 1'b0;
+     case (opcode)
             
-        end
-        4'b1100:begin
-            if (op1<op2) begin
-                out=16'b11;
+            5'b00000: begin
             end
-            else begin
-                out=16'b0;
+            5'b00001: begin
+                out = op1 + 1'b1 ;
             end
-            cmp_flag=1'b1;
-            
-        end
-        4'b1101:begin//shifting
-            out=op1>>1;
-            shift_flag=1'b1;
-        end
-        4'b1110:begin
-            out=op1<<1;
-            shift_flag=1'b1;
-        end
-        default:begin
-            out=16'b0;
-        end
+            5'b00010: begin
+            end
+            5'b00011: begin
+                out = op1 - 1'b1 ;
+            end
+            5'b00100: begin
+                {cf, out} = op1 + op2 ;   
+            end
+            5'b00101: begin
+                {cf, out} = op1 + op2 + cin ;
+            end
+            5'b00110: begin
+                {cf, out} = op1 - op2;
+            end
+            5'b00111: begin
+                {cf, out} = op1 - op2 - cin;
+            end
 
-    endcase
+            5'b01000: begin
+                out = op1 & op2 ;
+            end
+            5'b01001: begin
+                out = op1 | op2 ;
+            end
+            5'b01010: begin
+                out = op1 ^ op2 ;
+            end
+            5'b01011: begin
+                out = ~op1 ;
+            end
+            5'b01100: begin
+            end
+            5'b01101: begin
+            end
+            5'b01110: begin
+            end
+            5'b01111: begin
+            end
+
+            5'b10000: begin
+            end
+            5'b10001: begin
+            end
+            5'b10010: begin
+            end
+            5'b10011: begin
+            end
+            5'b10100: begin
+            end
+            5'b10101: begin
+            end
+            5'b10110: begin
+            end
+            5'b10111: begin
+            end
+
+            5'b11000: begin
+            end
+            5'b11001: begin
+            end
+            5'b11010: begin
+            end
+            5'b11011: begin
+            end
+            5'b11100: begin
+            end
+            5'b11101: begin
+            end
+            5'b11110: begin
+            end
+            5'b11111: begin
+            end
+            default: begin
+                out = 16'b0;
+                cf  = 1'b0;
+                zf  = 1'b0;
+                af  = 1'b0;
+                of  = 1'b0;
+                pf  = 1'b0;
+                nf  = 1'b0;
+            end
+
+        endcase
     end
 endmodule
